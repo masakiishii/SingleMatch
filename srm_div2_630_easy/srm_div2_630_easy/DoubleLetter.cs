b108b3954d86bf91868bc6b463f1689341ca14f2
masakiishii <masaki.ishii511@gmail.com>
2015-01-03 16:19:39 +0900

    add srm div2 630 easy

---
+++ srm_div2_630_easy/srm_div2_630_easy/DoubleLetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_630_easy
+{
+    class DoubleLetter
+    {
+        public String ableToSolve(String s)
+        {
+            Boolean flag = false;
+            while(!flag) {
+                flag = true;
+                for (int i = 0; i < s.Length; i++ )
+                {
+                    if(s[i].Equals(s[i + 1])) {
+                        s = s.Remove(i, 2);
+                        flag = false;
+                    }
+                }
+            }
+            return s.Length == 0 ? "Possible" : "Impossible";
+        }
+    }
+}
---
+++ srm_div2_630_easy/srm_div2_630_easy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_630_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLetter dubleletter = new DoubleLetter();
+            Console.WriteLine(dubleletter.ableToSolve("aabccb"));
+            Console.ReadLine();
+        }
+    }
+}
