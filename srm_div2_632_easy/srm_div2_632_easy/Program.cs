e337a16a43b1674d08efba406869674b01577161
masakiishii <masaki.ishii.private@gmail.com>
2015-01-04 17:39:16 +0900

    add srm div2 632 easy

---
+++ srm_div2_632_easy/srm_div2_632_easy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_632_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunningAroundPark running = new RunningAroundPark();
+
+            //int N = 3;
+            //int[] d = new int[] {1, 2, 3};
+            int N = 24;
+            int[] d = new int[] {6, 6};
+            //int N = 3;
+            //int[] d = new int[] {1, 2, 3};
+            //int N = 3;
+            //int[] d = new int[] {1, 2, 3};
+            //int N = 3;
+            //int[] d = new int[] {1, 2, 3};
+            int ans = running.numberOfLap(N, d);
+            Console.WriteLine(ans);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ srm_div2_632_easy/srm_div2_632_easy/RunningAroundPark.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_632_easy
+{
+    class RunningAroundPark
+    {
+        public int numberOfLap(int N, int[] d) {
+            int number = 1;
+            for (int i = 0; i < d.Length; i++ )
+            {
+                if((i != d.Length -1) && (d[i] >= d[i+1])) {
+                    number++;
+                }
+            }
+            return number;
+        }
+    }
+}
