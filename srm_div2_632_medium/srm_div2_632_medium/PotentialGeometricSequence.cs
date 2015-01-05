8add65e7db878c5f716aa3b57dc4cca06dd2f816
masakiishii <masaki.ishii.private@gmail.com>
2015-01-05 11:35:32 +0900

    add srm 632 div2 medium

---
+++ srm_div2_632_medium/srm_div2_632_medium/PotentialGeometricSequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_632_medium
+{
+    class PotentialGeometricSequence
+    {
+        public int numberOfSubsequences(int[] d)
+        {
+            int ans = d.Length;
+            int abs;
+            for (int i = 0; i < d.Length - 1; i++ )
+            {
+                abs = d[i + 1] - d[i];
+                ans++;
+                for (int j = i + 2; j < d.Length; j++ )
+                {
+                    int k = d[j] - d[j-1];
+                    if(k == abs) {
+                        ans++;
+                    }
+                    else if(k != abs) {
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ srm_div2_632_medium/srm_div2_632_medium/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_632_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PotentialGeometricSequence potential = new PotentialGeometricSequence();
+            int[] d = new int[] { 0, 1 ,2};
+            //int[] d = new int[] { 1, 2 ,4};
+            //int[] d = new int[] { 3, 2 , 1, 0};
+            //int[] d = new int[] { 1, 2, 4, 8, 16};
+            //int[] d = new int[] { 1, 3, 5, 5, 5, 5, 64, 4, 23, 2, 3, 4, 5, 4, 3};
+            Console.WriteLine(potential.numberOfSubsequence(d));
+            Console.ReadLine();
+        }
+    }
+}
