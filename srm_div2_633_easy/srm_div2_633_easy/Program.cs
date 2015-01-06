fc740818f700d5d29abe6be90c96c9c11064b53c
masakiishii <masaki.ishii.private@gmail.com>
2015-01-06 11:15:50 +0900

    add srm 633 div2 easy

---
+++ srm_div2_633_easy/srm_div2_633_easy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_633_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 13;
+            Target target = new Target();
+            string[] ans = target.draw(n);
+            for (int i = 0; i < n; i++ )
+            {
+                Console.WriteLine(ans[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ srm_div2_633_easy/srm_div2_633_easy/Target.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_633_easy
+{
+    class Target
+    {
+        public string[] draw(int n)
+        {
+            char[, ] coodination = new char[n, n];
+            string[] ans = new string[n];
+            for(int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++ )
+                {
+                    coodination[i, j] = ' ';
+                }
+            }
+            for(int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++ )
+                {
+                    if(Math.Max(Math.Abs(i - n / 2), Math.Abs(j - n / 2)) % 2 == 0 ) {
+                        coodination[i, j] = '#';
+                    }
+                }
+            }
+            for(int i = 0; i < n; i++) {
+                StringBuilder builder = new StringBuilder();
+                for (int j = 0; j < n; j++ )
+                {
+                    builder.Append(coodination[i, j]);
+                }
+                ans[i] = builder.ToString();
+            }
+            return ans;
+        }
+    }
+}
