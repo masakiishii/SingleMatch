da86057879c2cb2f1d540c1151623c8ee3a93293
masakiishii <masaki.ishii.private@gmail.com>
2015-01-07 21:20:51 +0900

    add srm div2 634 easy

---
+++ srm_div2_634_easy/srm_div2_634_easy/MountainRanges.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_634_easy
+{
+    class MountainRanges
+    {
+        public int countPeeks(int[] heights)
+        {
+            int counter = 0;
+            if(heights.Length == 1) {
+                return 1;
+            }
+            else {
+                if(heights[0] > heights[1]) {
+                    counter++;
+                }
+                for (int i = 1; i < heights.Length - 1; i++ )
+                {
+                    if(heights[i - 1] < heights[i] && heights[i] > heights[i+1]) {
+                        counter++;
+                    }
+                }
+                if(heights[heights.Length - 2] < heights[heights.Length - 1]) {
+                    counter++;
+                }
+                return counter;
+            }
+        }
+    }
+}
---
+++ srm_div2_634_easy/srm_div2_634_easy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_634_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] d = new int[] { 1, 2, 3, 4, 4, 3, 2, 1};
+            MountainRanges check = new MountainRanges();
+            Console.WriteLine(check.countPeeks(d));
+            Console.ReadLine();
+        }
+    }
+}
