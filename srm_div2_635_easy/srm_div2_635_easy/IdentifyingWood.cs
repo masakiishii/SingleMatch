cf5a3215a3fcc1820ce87b2430be7c683a31b8ff
masakiishii <masaki.ishii.private@gmail.com>
2015-01-08 11:40:22 +0900

    add srm div2 635 easy

---
+++ srm_div2_635_easy/srm_div2_635_easy/IdentifyingWood.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_635_easy
+{
+    class IdentifyingWood
+    {
+        public String check(String s, String t)
+        {
+            int index = 0;
+            int tlength = t.Length;
+            for (int i = 0; i < s.Length; i++ )
+            {
+                if(s[i] == t[index]) {
+                    index++;
+                }
+                if(tlength == index) {
+                    return "Yes, it's wood.";
+                }
+            }
+            return "Nope.";
+        }
+    }
+}
---
+++ srm_div2_635_easy/srm_div2_635_easy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_635_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IdentifyingWood idWood = new IdentifyingWood();
+            Console.WriteLine(idWood.check("absdefgh", "asdf"));
+            Console.WriteLine(idWood.check("oxoxoxox", "ooxxoo"));
+            Console.WriteLine(idWood.check("oxoxoxox", "xxx"));
+            Console.WriteLine(idWood.check("qwerty", "qwerty"));
+            Console.WriteLine(idWood.check("string", "longstring"));
+            Console.ReadLine();
+        }
+    }
+}
