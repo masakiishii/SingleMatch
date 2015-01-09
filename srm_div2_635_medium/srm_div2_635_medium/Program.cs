a18ed64d8c2cdecb3b4ecb90a893f57cbb760041
masakiishii <masaki.ishii.private@gmail.com>
2015-01-09 18:31:30 +0900

    add srm div2 635 medium

---
+++ srm_div2_635_medium/srm_div2_635_medium/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_635_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticLaw ql = new QuadraticLaw();
+            Console.WriteLine(ql.getTime(1));
+            Console.WriteLine(ql.getTime(2));
+            Console.WriteLine(ql.getTime(5));
+            Console.WriteLine(ql.getTime(6));
+            Console.WriteLine(ql.getTime(7));
+            Console.WriteLine(ql.getTime(1482));
+            Console.WriteLine(ql.getTime(1000000000000000000));
+            Console.WriteLine(ql.getTime(31958809614643170));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ srm_div2_635_medium/srm_div2_635_medium/QuadraticLaw.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_635_medium
+{
+    class QuadraticLaw
+    {
+        public long getTime(long d)
+        {
+            long t = (long)Math.Sqrt(d);
+            while(t * t + t > d) {
+                t--;
+            }
+            return t;
+        }
+    }
+}
