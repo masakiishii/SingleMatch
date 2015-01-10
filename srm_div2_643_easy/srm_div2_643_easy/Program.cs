a362284e17d4376eee309f0b75929aeecf9af1ad
masakiishii <masaki.ishii.private@gmail.com>
2015-01-10 22:54:50 +0900

    add srm div2 643 easy

---
+++ srm_div2_643_easy/srm_div2_643_easy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_643_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheKingsArmyDiv2 army = new TheKingsArmyDiv2();
+            //String[] grid = new String[] {
+            //    "SSSSS",
+            //    "SSHHS",
+            //    "SSSSS"
+            //};
+            String[] grid = new String[] {
+                "SSSSS",
+                "SSHSH",
+                "HSSSS"
+            };
+            Console.WriteLine(army.getNumber(grid));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ srm_div2_643_easy/srm_div2_643_easy/TheKingsArmyDiv2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_643_easy
+{
+    class TheKingsArmyDiv2
+    {
+        public int getNumber(String[] state)
+        {
+            Boolean flag1 = false;
+            Boolean flag2 = false;
+            for (int i = 0; i < state.Length; i++ )
+            {
+                for (int j = 0; j < state[0].Length; j++ )
+                {
+                    if(state[i][j] == 'H') {
+                        flag1 = true;
+                        if (j + 1 < state[0].Length && state[i][j].Equals(state[i][j + 1]))
+                        {
+                            flag2 = true;
+                        }
+                        if (i + 1 < state.Length && state[i][j].Equals(state[i + 1][j]))
+                        {
+                            flag2 = true;
+                        }
+                    }
+                }
+            }
+            if(flag1 && flag2) {
+                return 0;
+            }
+            if(flag1) {
+                return 1;
+            }
+            return 2;
+        }
+    }
+}
