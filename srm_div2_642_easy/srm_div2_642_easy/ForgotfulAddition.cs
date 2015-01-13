3bcc75f185594c264d9d1ce8f09197034e5f912a
masakiishii <masaki.ishii.private@gmail.com>
2015-01-13 23:12:54 +0900

    add srm div2 642 easy

---
+++ srm_div2_642_easy/srm_div2_642_easy/ForgotfulAddition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_642_easy
+{
+    class ForgotfulAddition
+    {
+        public int minNumber(String expression)
+        {
+            int ans  = int.MaxValue;
+            int pre  = 0;
+            int post = 0;
+            int sum  = 0; 
+            for (int i = 1; i < expression.Length; i++ )
+            {
+                pre  = int.Parse(expression.Substring(0, i));
+                post = int.Parse(expression.Substring(i));
+                sum = pre + post;
+                if(ans > sum) {
+                    ans = sum;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ srm_div2_642_easy/srm_div2_642_easy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_642_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForgotfulAddition fa = new ForgotfulAddition();
+            Console.WriteLine(fa.minNumber("22"));
+            Console.WriteLine(fa.minNumber("123"));
+            Console.WriteLine(fa.minNumber("1289"));
+            Console.WriteLine(fa.minNumber("31415926"));
+            Console.WriteLine(fa.minNumber("98765"));
+            Console.ReadLine();
+        }
+    }
+}
