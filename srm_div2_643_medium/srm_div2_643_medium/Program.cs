2f2ba135c2b7f08b9c556c66fb56ee9e8bb08556
masakiishii <masaki.ishii.private@gmail.com>
2015-01-14 01:35:24 +0900

    add srm div2 643 medium

---
+++ srm_div2_643_medium/srm_div2_643_medium/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_643_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TheKingsFactorization fact = new TheKingsFactorization();
+            //long n = 12;
+            //long[] primes1 = new long[]{2, 3};
+            //long[] ans = fact.getVector(n, primes1);
+
+
+            //long n = 7;
+            //long[] primes2 = new long[]{7};
+            //long[] ans = fact.getVector(n, primes2);
+
+
+            //long n = 1764;
+            //long[] primes3 = new long[]{2, 3, 7};
+            //long[] ans = fact.getVector(n, primes3);
+
+
+            //long n = 49;
+            //long[] primes4 = new long[]{7};
+            //long[] ans = fact.getVector(n, primes4);
+
+            //long n = 210;
+            //long[] primes5 = new long[]{2, 5};
+            //long[] ans = fact.getVector(n, primes5);
+
+            long n = 100000;
+            long[] primes6 = new long[]{2, 2, 2, 5, 5};
+            long[] ans = fact.getVector(n, primes6);
+
+            for (int i = 0; i < ans.Length; i++ )
+            {
+                Console.Write(ans[i] + ", ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ srm_div2_643_medium/srm_div2_643_medium/TheKingsFactorization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_643_medium
+{
+    class TheKingsFactorization
+    {
+        public long[] getVector(long N, long[] primes)
+        {
+            List<long> anslist = new List<long>();
+            for (int i = 0; i < primes.Length; i++ )
+            {
+                N /= primes[i];
+                anslist.Add(primes[i]);
+            }
+            for (int i = 0; i < primes.Length - 1; i++ )
+            {
+                for (long x = primes[i]; x <= primes[i + 1]; x++)
+                {
+                    if (N % x == 0)
+                    {
+                        N /= x;
+                        anslist.Add(x);
+                        break;
+                    }
+                }
+            }
+            if(N > 1) {
+                anslist.Add(N);
+            }
+            anslist.Sort();
+            return anslist.ToArray();
+        }
+    }
+}
