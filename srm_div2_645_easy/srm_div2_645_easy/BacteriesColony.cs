10019261f112519302df1e8102dc2b541c5afee1
masakiishii <masaki.ishii.private@gmail.com>
2015-01-15 11:49:21 +0900

    add Program.cs and BacteriesColony.cs

---
+++ srm_div2_645_easy/srm_div2_645_easy/BacteriesColony.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_645_easy
+{
+    class BacteriesColony
+    {
+        public int[] performTheExperiment(int[] colonies)
+        {
+            int [] answercolonies = new int[colonies.Length];
+            colonies.CopyTo(answercolonies, 0);
+            Boolean flag = true;
+            while(flag) {
+                flag = false;
+                for (int i = 1; i < colonies.Length - 1; i++ )
+                {
+                    if(colonies[i - 1] > colonies[i] && colonies[i] < colonies[i + 1]) {
+                        flag = true;
+                        answercolonies[i]++;
+                    }
+                    if(colonies[i - 1] < colonies[i] && colonies[i] > colonies[i + 1]) {
+                        flag = true;
+                        answercolonies[i]--;
+                    }
+                }
+                answercolonies.CopyTo(colonies, 0);
+            }
+            return answercolonies;
+        }
+    }
+}
---
+++ srm_div2_645_easy/srm_div2_645_easy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_645_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] colony = new int[] {5, 3, 4, 6, 1 };
+            //int[] colony = new int[] {1, 5, 4, 9};
+            //int[] colony = new int[] {78, 34, 3, 54, 44, 99};
+            int[] colony = new int[] {32, 68, 50, 89, 34, 56, 47, 30, 82, 7, 21, 16, 82, 24, 91};
+            BacteriesColony bc = new BacteriesColony();
+            int[] ans = bc.performTheExperiment(colony);
+            for (int i = 0; i < ans.Length; i++ )
+            {
+                Console.Write(ans[i] + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
