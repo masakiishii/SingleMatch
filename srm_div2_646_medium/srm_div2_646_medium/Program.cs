1933d6c020a9fdf6d303d85db53916a1b69a5b47
masakiishii <masaki.ishii.private@gmail.com>
2015-01-17 00:18:14 +0900

    add srm div2 646 medium

---
+++ srm_div2_646_medium/srm_div2_646_medium/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_646_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] x = new int[] {1, 1, 1, 1};
+            // int[] y = new int[] {-2, -1, 0, 1};
+            // int k = 4;
+            // int[] x = new int[] {};
+            // int[] y = new int[] {};
+            // int k = 1000;
+             int[] x = new int[] {-1, 0, 0, 1};
+             int[] y = new int[] {0, -1, 1, 0};
+             int k = 9;
+            //int[] x = new int[] {1,0,0,-1,-1,-2,-2,-3,-3,-4,-4};
+            //int[] y = new int[] {0,-1,1,-2,2,-3,3,-4,4,-5,5};
+            //int k = 47;
+            TheGridDivTwo solve = new TheGridDivTwo();
+            Console.WriteLine(solve.find(x, y, k));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ srm_div2_646_medium/srm_div2_646_medium/TheGridDivTwo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_646_medium
+{
+    public class TheGridDivTwo
+    {
+        private int[] dx = new int[] {1, 0, -1, 0 };
+        private int[] dy = new int[] {0, 1,  0, -1};
+        private int[, ] distance = new int[2000, 2000];
+        public int find(int[] x, int[] y, int k)
+        {
+            if(x.Length == 0) {
+                return k;
+            }
+            int ans = 0;
+            for (int i = 0; i < x.Length; i++ )
+            {
+                x[i] += 1000;
+                y[i] += 1000;
+                distance[x[i], y[i]] = -1;
+            }
+            distance[1000, 1000] = -1;
+            Queue<Tuple<int, int>> queue = new Queue<Tuple<int, int>>();
+            Tuple<int, int> start = new Tuple<int, int>(1000, 1000);
+            queue.Enqueue(start);
+            while(queue.Count != 0) {
+                Tuple<int, int> tuple = queue.Dequeue();
+                for (int i = 0; i < 4; i++ )
+                {
+                    int posX = tuple.Item1 + dx[i];
+                    int posY = tuple.Item2 + dy[i];
+                    if(0 <= posX && posX < 1000 + k && 0 <= posY && posY < 1000 + k && distance[posX, posY] == 0) {
+                        distance[posX, posY] = distance[tuple.Item1, tuple.Item2] + 1;
+                        if(distance[posX, posY] == k) {
+                            ans = Math.Max(ans, posX);
+                        }
+                        if(distance[posX, posY] < k) {
+                            Tuple<int, int> pair = new Tuple<int, int>(posX, posY);
+                            queue.Enqueue(pair);
+                        }
+                    }
+                }
+            }
+            return ans == 0 ? 0 : ans - 1001;
+        }
+    }
+}
