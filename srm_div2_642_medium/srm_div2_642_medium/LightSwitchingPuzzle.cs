cd5c6c96f9ed57094459ac84c53b102c5649670e
masakiishii <masaki.ishii.private@gmail.com>
2015-01-18 16:41:23 +0900

    add srm div2 642 medium

---
+++ srm_div2_642_medium/srm_div2_642_medium/LightSwitchingPuzzle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_642_medium
+{
+    public class LightSwitchingPuzzle
+    {
+        public int minFlips(String state)
+        {
+            bool[] boolstate = new bool[state.Length];
+            int ans = 0;
+            for (int i = 0; i < state.Length; i++)
+            {
+                boolstate[i] = state[i].Equals('Y') ? true : false;
+            }
+            for (int i = 0; i < boolstate.Length; i++)
+            {
+                if (boolstate[i])
+                {
+                    ans++;
+                    for (int j = i; j < boolstate.Length; j += (i + 1))
+                    {
+                        boolstate[j] = boolstate[j] ? false : true;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ srm_div2_642_medium/srm_div2_642_medium/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_642_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LightSwitchingPuzzle lsp = new LightSwitchingPuzzle();
+            String state = "YYYYYY";
+            Console.WriteLine(lsp.minFlips(state));
+            state = "YNYNYNYNY";
+            Console.WriteLine(lsp.minFlips(state));
+            state = "NNNNNNNNNN";
+            Console.WriteLine(lsp.minFlips(state));
+            state = "YYYNYYYNYYYNYYNYYYYN";
+            Console.WriteLine(lsp.minFlips(state));
+            state = "NYNNYNNNYNNNNYNNNNNYNNNNNNYNNNNNNNY";
+            Console.WriteLine(lsp.minFlips(state));
+            Console.ReadLine();
+        }
+    }
+}
