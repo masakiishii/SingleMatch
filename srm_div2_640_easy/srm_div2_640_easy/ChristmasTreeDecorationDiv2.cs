25935852e665bb02fae421d6de4e914c0ca2c4b3
masakiishii <masaki.ishii.private@gmail.com>
2015-01-21 19:42:10 +0900

    add srm div2 640 easy

---
+++ srm_div2_640_easy/srm_div2_640_easy/ChristmasTreeDecorationDiv2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_640_easy
+{
+    public class ChristmasTreeDecorationDiv2
+    {
+        public int solve(int[] col, int[] x, int[] y) {
+            int ans = 0;
+            for (int i = 0; i < x.Length; i++ )
+            {
+                if(col[x[i] - 1 ] != col[y[i] - 1]) {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ srm_div2_640_easy/srm_div2_640_easy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_640_easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChristmasTreeDecorationDiv2 cl = new ChristmasTreeDecorationDiv2();
+            // int[] col = new int[]{1,2,3,3};
+            // int[] x   = new int[]{1,2,3};
+            // int[] y   = new int[]{2,3,4};
+            // Console.WriteLine(cl.solve(col, x, y));
+
+            // int[] col = new int[]{1,3,5};
+            // int[] x   = new int[]{1,3};
+            // int[] y   = new int[]{2,2};
+            // Console.WriteLine(cl.solve(col, x, y));
+
+            // int[] col = new int[]{1,1,3,3};
+            // int[] x   = new int[]{1,3,2};
+            // int[] y   = new int[]{2,1,4};
+            // Console.WriteLine(cl.solve(col, x, y));
+
+            // int[] col = new int[]{5,5,5,5};
+            // int[] x   = new int[]{1,2,3};
+            // int[] y   = new int[]{2,3,4};
+            // Console.WriteLine(cl.solve(col, x, y));
+
+            int[] col = new int[]{9,1,1};
+            int[] x   = new int[]{3,2};
+            int[] y   = new int[]{2,1};
+            Console.WriteLine(cl.solve(col, x, y));
+
+            Console.ReadLine();
+        }
+    }
+}
