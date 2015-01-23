81de4e88129cec4fe039b07d4a01d4f6d00f97aa
masakiishii <masaki.ishii.private@gmail.com>
2015-01-24 00:08:07 +0900

    add srm div2 639 medium

---
+++ srm_div2_639_medium/srm_div2_639_medium/AliceGameEasy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_639_medium
+{
+    public class AliceGameEasy
+    {
+        public long findMinimumValue(long x, long y)
+        {
+            long sum = x + y;
+            long count = 1;
+            long ans = 0;
+            while(sum > 0) {
+                sum -= count++;
+            }
+            if(sum < 0) {
+                return -1;
+            }
+            count -= 1;
+            while(count > 0) {
+                if(count <= x) {
+                    x -= count;
+                    ans++;
+                }
+                count--;
+            }
+            return x == 0 ? ans : -1;
+        }
+    }
+}
---
+++ srm_div2_639_medium/srm_div2_639_medium/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_639_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = 7;
+            long y = 14;
+            AliceGameEasy alice = new AliceGameEasy();
+            Console.WriteLine(alice.findMinimumValue(x, y));
+            x = 10;
+            y = 0;
+            Console.WriteLine(alice.findMinimumValue(x, y));
+            x = 932599670050;
+            y = 67400241741;
+            Console.WriteLine(alice.findMinimumValue(x, y));
+            x = 7;
+            y = 13;
+            Console.WriteLine(alice.findMinimumValue(x, y));
+            x = 0;
+            y = 0;
+            Console.WriteLine(alice.findMinimumValue(x, y));
+            x = 100000;
+            y = 400500;
+            Console.WriteLine(alice.findMinimumValue(x, y));
+            Console.ReadLine();
+        }
+    }
+}
