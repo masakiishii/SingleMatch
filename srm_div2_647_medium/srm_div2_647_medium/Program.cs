7b42f160872c7053aa9fa1b370bf1841d6b04731
masakiishii <masaki.ishii.private@gmail.com>
2015-02-01 16:41:42 +0900

    add srm div2 647 medium

---
+++ srm_div2_647_medium/srm_div2_647_medium/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_647_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ srm_div2_647_medium/srm_div2_647_medium/TravellingSalesmanEasy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_647_medium
+{
+    public class TravellingSalesmanEasy
+    {
+        public int getMaxProfit(int M, int[] profit, int[] city, int[] visit)
+        {
+            return -1;
+        }
+    }
+}
