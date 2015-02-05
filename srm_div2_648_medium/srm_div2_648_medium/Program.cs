50c2406b8803690350e7ee87ad2da7cee1ee50a8
masakiishii <masaki.ishii.private@gmail.com>
2015-02-05 23:24:40 +0900

    add srm div2 648 medium

---
+++ srm_div2_648_medium/srm_div2_648_medium/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srm_div2_648_medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] graph = new String[10];
+            Fragile2.countPairs(graph);
+            Console.ReadLine();
+        }
+    }
+
+    public class Fragile2
+    {
+        public static int countPairs(String[] graph)
+        {
+            Console.WriteLine("countPaire");
+            return -1;
+        }
+    }
+}
